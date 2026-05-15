a712be8d8079732d05973b4189118a759484c8e0
Hey macha github.io <muhajirnasarudin@gmail.com>
2026-05-15 19:19:32 +0700

    Update navbar.html

--- firebase.js
+++ firebase.js
@@ -1,13 +1,47 @@
 const firebaseConfig = {
+
   apiKey: "AIzaSyA5ihXVlLL2NqwM2mwjp-ZMmcOT9Bs78wE",
+
   authDomain: "esteh-online.firebaseapp.com",
+
   projectId: "esteh-online",
+
   storageBucket: "esteh-online.firebasestorage.app",
+
   messagingSenderId: "662542044878",
+
   appId: "1:662542044878:web:8b0c0abf72f9f168530a9f",
+
   measurementId: "G-5C3P2T0L9T"
+
 };
+
+/* INITIALIZE FIREBASE */
 
 firebase.initializeApp(firebaseConfig);
 
+/* AUTH */
+
 const auth = firebase.auth();
+
+/* FIRESTORE DATABASE */
+
+const db = firebase.firestore();
+
+/* CHECK LOGIN */
+
+auth.onAuthStateChanged((user) => {
+
+  if(user){
+
+    console.log("Login:", user.email);
+
+  }
+
+  else{
+
+    console.log("Belum login");
+
+  }
+
+});

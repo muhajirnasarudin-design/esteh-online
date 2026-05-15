5371ea56f134922aefed82d15fa646a1025acfcf
Hey macha github.io <muhajirnasarudin@gmail.com>
2026-05-15 09:51:04 +0700

    Update navbar.html

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "123456",
-  appId: "APP_ID"
+  apiKey: "AIzaSyA5ihXVlLL2NqwM2mwjp-ZMmcOT9Bs78wE",
+  authDomain: "esteh-online.firebaseapp.com",
+  projectId: "esteh-online",
+  storageBucket: "esteh-online.firebasestorage.app",
+  messagingSenderId: "662542044878",
+  appId: "1:662542044878:web:8b0c0abf72f9f168530a9f",
+  measurementId: "G-5C3P2T0L9T"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const db = firebase.firestore();
